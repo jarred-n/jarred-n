52d9034fdbbde6879f7d24bda407fbe09feea626
hong焕佳 <hhuanjia@163.com>
2019-10-30 18:37:52 +0800

    提交layout组件

---
+++ src/components/common/autoBackToTop/index.tsx
@@ -0,0 +1,1 @@
+@components/common/autoBackToTop/node_modules/react@components/common/autoBackToTop/node_modules/react
--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -1,11 +1,22 @@
 import React, { Component } from 'react'
+import Routers from '../Routers'
+import Header from '@components/common/header'
 
 export default class Layouts extends Component {
+  constructor(props: any) {
+    super(props)
+    this.state = {}
+  }
+
   render() {
     return (
-      <div>
-        haha
-      </div>
+    //   <AutoBackToTop>
+        <div>
+            <Header />
+            <Routers />
+        </div>
+        
+    //   </AutoBackToTop>
     )
   }
 }
