506e87f10464cf784a33de89f92d27ca6be060ac
hong焕佳 <hhuanjia@163.com>
2019-11-12 19:40:06 +0800

    博客也修改

--- src/pages/blog/index.tsx
+++ src/pages/blog/index.tsx
@@ -1,11 +1,15 @@
 import React, { Component } from 'react'
 import { observer, inject } from 'mobx-react';
 import { ArticleProps } from '../../types/article';
+import Pagination from 'rc-pagination';
 import cs from 'classnames'
 import history from '../../history';
 import Title from '@components/common/title'
 import styles from './index.module.scss';
+import { Link } from 'react-router-dom';
 import { webpSuffix, blogBg } from '../../constants/constants';
+import routePath from '@constants/routePath';
+import PostSummary from '@components/post/postSummary/index';
 
 
 @inject('articleStore')
@@ -42,6 +46,28 @@ export default class Blog extends Component<ArticleProps, {}> {
         >
             <span>Tech, Travel and Life</span>
         </figure>
+        <div className={styles.main_content}>
+            <section>
+                {
+                    !articleStore!.isSummaryLoading && articleStore!.posts.length === 0? 
+                    (
+                        <div>
+                            <p className={styles.no_articles}>暂无发表文章!</p>
+                            <Link to={routePath.blog} className={styles.back}>
+                                返回
+                            </Link>
+                        </div>
+                    ) : (
+                        <PostSummary />
+                    )
+                }
+                {
+                    history.location.pathname.includes('blog')? (
+                        <Pagination />
+                    ) : null
+                }
+            </section>
+        </div>
       </main>
     )
   }
